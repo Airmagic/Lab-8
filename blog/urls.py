770f2e056c4f2deed6d284a8ecbaae3cb3be28d9
Airmagic <babaphat@yahoo.com>
2018-03-24 16:20:43 -0500

    added a details page

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 # this is the url pattern
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+# getting render and 404
+from django.shortcuts import render, get_object_or_404
 # added this to get utilities
 from django.utils import timezone
 # added this to access post
@@ -10,3 +11,8 @@ def post_list(request):
     # this is for posting
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+# this is for the details page
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
