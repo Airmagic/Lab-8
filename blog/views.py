945e8ddd3a61ab6c58df746580d01f2e0f11cad4
Airmagic <babaphat@yahoo.com>
2018-03-24 15:08:44 -0500

    <div>     <h1><a href="/">Django Girls Blog</a></h1> </div>
    {% for post in posts %}
        <div>
            <p>published: {{ post.published_date }}</p>
            <h1><a href="">{{ post.title }}</a></h1>
            <p>{{ post.text|linebreaksbr }}</p>
        </div>
    {% endfor %}

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+# added this to get utilities
+from django.utils import timezone
+# added this to access post
+from .models import Post
 
 # Create your views here.
 # this will render the template
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # this is for posting
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
